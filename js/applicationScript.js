1a95f12df74e91679911f8c479d168c782613d23
CAEUser <mail@rwth>
2021-02-08 14:28:10 +0000

    MicroserviceCall edited by Philipp Dolif

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -60,7 +60,8 @@ var loadSongs = function(){
 
   client.sendRequest("GET", "songs", "", "", {}, false,
   function(data, type) {
-    console.log(data);
+    console.log(data); 
+    document.getElementById("p").innerText = data.map(song => song.title);
   },
   function(error) {
     console.log(error);
